ae2ef35e193a1d1eff180c8e27eea092888ec765
Fatima Idrees <fatimaidrees334@gmail.com>
2025-04-27 17:39:05 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+let weight=parseFloat(prompt("Enter your weight in kilograms(kg)"));
+let height=parseFloat(prompt("Enter your height in meters(m)"));
+let BMI=weight/height^2;
+console.log(`Your BMI is ${BMI}`);
